886eb5d77cfc6cdd39e109cb15193e555f421d00
Kailash Sharma <>
2023-12-29 18:13:38 +0530

    docker updated

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
+
 set -e
+
+grep -qxF '185.199.108.133 raw.githubusercontent.com' /etc/hosts || bash -c 'echo "185.199.108.133 raw.githubusercontent.com" >> /etc/hosts'
+
+# Check if .env file exists
+if [ -e .env ]; then
+    echo ".env file already exists. Doing nothing."
+else
+    # Copy .env.dist to .env
+    cp .env.dist .env
+    echo ".env file created by copying .env.dist."
+fi
 
 timestamp()
 {
  date +"%Y-%m-%d %T"
 }
-
+composer install
 runConsoleSymfonyCommand () {
     echo "$(timestamp):[run] php bin/console $1"
     output=`php bin/console $1`
@@ -27,6 +39,9 @@ else
 fi
 
 runConsoleSymfonyCommand "cache:clear"
-
+runConsoleSymfonyCommand "lexik:jwt:generate-keypair --skip-if-exists"
+runConsoleSymfonyCommand "d:m:m -n"
+yarn install
+yarn encore dev
 echo "$(timestamp):[run] Running supervisord";
 /usr/bin/supervisord -c ./docker/config/supervisord.conf
